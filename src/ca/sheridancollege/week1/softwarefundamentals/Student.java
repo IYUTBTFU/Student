7a83d59f5058cae16fec751a8acfdcc8b994b38b
IYUTBTFU <browtrey@sheridancollege.ca>
2020-01-15 10:28:42 -0500

    2nd commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,25 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
+    private double GPA;
+    private String eMail;
 
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+    
     /**
      * Get the value of name
      *
